cd8183a7b240bdfc491951a6cca1c92eb143c448
rmacuna <rmacuna@uninorte.edu.co>
2019-02-23 22:19:00 -0500

    Listo

--- src/app/perfomance/perfomance.component.ts
+++ src/app/perfomance/perfomance.component.ts
@@ -15,25 +15,49 @@ export class PerfomanceComponent implements OnInit {
   // Modelo que inicia la vista inicialmente en falso para que el test se muestre al dar click. 
   perfomanceActive: boolean;
 
+
+  DEVICE_INFO: any;
+  INFORMATION: any;
+  DEFAULT_NUMBER = 150;
+
   constructor(
     private progress: NgProgress,
     private render: Renderer2,
   ) { }
 
   ngOnInit() {
+    this.INFORMATION = [];
     this.perfomanceActive = false;
   }
 
 
-  startPerfomanceTest(element: HTMLElement) {
+  startPerfomanceTest() {
     this.progress.start();
+    this.DEVICE_INFO = this.getDeviceInfo();
+
     setTimeout(() => {
+      for (let i = 1; i <= this.DEFAULT_NUMBER; i++) {
+        this.INFORMATION.push('1');
+      }
       this.perfomanceActive = true;
-      this.render.addClass(this.elem.nativeElement, 'hidden');
+      this.render.removeClass(this.elem.nativeElement, 'hidden');
       this.render.addClass(this.elem.nativeElement, 'show');
-      this.elem.nativeElement.scrollIntoView({behavior: 'smooth'});
+      this.elem.nativeElement.scrollIntoView({ behavior: 'smooth' });
       this.progress.done();
-    }, 1500);
+    }, 1000);
+  }
+
+  // Obtiene información del equipo en el que se esta haciendo el benchmark.
+  private getDeviceInfo() {
+    const INFO = [
+      'Browser codename: ' + navigator.appCodeName,
+      'Browser name: ' + navigator.appName,
+      'Browser version: ' + navigator.appVersion,
+      'Cookies Enabled: ' + navigator.cookieEnabled,
+      'Platform:' + navigator.platform,
+      'User-agent header: ' + navigator.userAgent
+    ];
+    return INFO;
   }
 
 }

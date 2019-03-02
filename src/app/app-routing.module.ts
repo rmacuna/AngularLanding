15c36a82d3dfadb7d3eaf97b0c11f13323c5e036
rmacuna <rmacuna@uninorte.edu.co>
2019-03-01 19:10:42 -0500

    Implemented lazy loading for optimize the initial load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,23 @@ import { HomeComponent } from './home/home.component';
 import { PerfomanceComponent } from './perfomance/perfomance.component';
 
 const routes: Routes = [
+    // {
+    //   path: '',
+    //   pathMatch: 'full',
+    //   component: HomeComponent,
+    // },
     {
-      path: '',
-      pathMatch: 'full',
-      component: HomeComponent,
+      path: 'landing',
+      loadChildren: './landing/landing.module#LandingModule',
     },
     {
       path: 'perfomanceTest',
       component: PerfomanceComponent
+    },
+    {
+      path: '',
+      redirectTo: 'landing',
+      pathMatch: 'full'
     }
 ];
 
